aea65d900d95e16d7db565337c5643b5fec5e2fa
Rebecca Jackson <zimbecs@gmail.com>
2019-04-24 10:50:04 +1200

    dynamic data, removed dummy data

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../models/Todo'
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/Todo';
+
 
 @Component({
   selector: 'app-todos',
@@ -9,26 +12,16 @@ import { Todo } from '../../models/Todo'
 export class TodosComponent implements OnInit {
   todos: Todo[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      },
-    ];
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
+  }
 }
