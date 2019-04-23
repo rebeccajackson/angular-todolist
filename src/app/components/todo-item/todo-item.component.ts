f51d8d832f3781519217a66338fde8d58a93a3f5
Rebecca Jackson <zimbecs@gmail.com>
2019-04-24 10:49:38 +1200

    toggle and delete todo functions

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,8 +9,9 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
@@ -22,5 +24,16 @@ export class TodoItemComponent implements OnInit {
     };
 
     return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    });
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
   }
 }
